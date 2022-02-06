c0a1d78363e6c0506f0f3a34723b2dd0e6c43531
brokenfingers <buvotrys@gmail.com>
2022-02-07 00:36:44 +0200

    layoutKeyboard

--- js/js.js
+++ js/js.js
@@ -1,4 +1,25 @@
+let keyboardString = "1234567890qwertyuiopasdfghjklzxcvbnm";
 let menu = [];
+
+function layoutKeyboard(string, keyboardParameter) {
+    const keyboard = document.querySelector('.keyboard');
+    let lastValue = 0;
+    for (let n = 0; n < keyboardParameter.length; n++) {
+        let row = document.createElement('div');
+        row.classList.add("keyboard-row");
+        keyboard.appendChild(row)
+        for (let i = lastValue; i < keyboardParameter[n]; i++) {
+            const keyBtn = document.createElement('div');
+            keyBtn.textContent = string[i];
+            row.appendChild(keyBtn);
+        }
+        lastValue = keyboardParameter[n];
+    }
+
+}
+
+layoutKeyboard(keyboardString, [10, 20, 29, 36]);
+
 async function loadMenu() {
     const requestUrl = './data/menu.json';
     const request = new Request(requestUrl);
@@ -10,17 +31,23 @@ async function loadMenu() {
 
 function createMenu(obj) {
     const pizzaMenu = document.querySelector(".pizza-menu");
+    const sushiMenu = document.querySelector(".sushi-menu");
     for (itm of obj) {
         const li = document.createElement("li");
         li.textContent = itm.name;
         li.setAttribute("id", itm.id);
-        li.addEventListener("click", () => console.log(this), true);
-        pizzaMenu.appendChild(li);
+        li.addEventListener("click", onMenu);
+        if (itm.type == "pizza") {
+            pizzaMenu.appendChild(li);
+        } else {
+            sushiMenu.appendChild(li);
+        }
+
     }
 }
 
 function onMenu() {
-
+    console.log(this.id);
 }
 
 loadMenu();

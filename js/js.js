1828e2f14fa1b56cd2039302bcea6b2744c49ede
brokenfingers <buvotrys@gmail.com>
2022-02-06 23:28:53 +0200

    json array import from file

--- js/js.js
+++ js/js.js
@@ -1,1 +1,26 @@
-console.log('code');
+let menu = [];
+async function loadMenu() {
+    const requestUrl = './data/menu.json';
+    const request = new Request(requestUrl);
+
+    const response = await fetch(request);
+    menu = await response.json();
+    createMenu(menu);
+}
+
+function createMenu(obj) {
+    const pizzaMenu = document.querySelector(".pizza-menu");
+    for (itm of obj) {
+        const li = document.createElement("li");
+        li.textContent = itm.name;
+        li.setAttribute("id", itm.id);
+        li.addEventListener("click", () => console.log(this), true);
+        pizzaMenu.appendChild(li);
+    }
+}
+
+function onMenu() {
+
+}
+
+loadMenu();
